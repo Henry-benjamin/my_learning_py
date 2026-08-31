4823bb4c978971b3dd8a10fd060cafbc09b73b40
henry-benjamin <akyambahenrybenjamin@gmail.com>
2026-08-31 23:33:52 +0300

    If statement plus one challange

--- app.py
+++ app.py
@@ -78,4 +78,28 @@ x = 10 + 3 * 2 # 16
 x = 2.9
 round(x)# it rounds to the nearest integer(3)
 abs(-2.9)# always returns a positive number not matter what 
-import math # to perform well math we import math then use all its methods
+import math # to perform well math we import math then use all its functions
+
+#6 IF STATEMENTS
+is_hot = False
+is_cold = False
+
+#if is_hot:
+    #print("It's a hot day")
+    #print("Drink plenty of water")
+#elif is_cold:
+    #print("It's cold day")
+    #print("Wear warm clothers")
+#else:
+    #print("It's a lovely day")
+#print("Enjoy your day")
+
+#CHALLANGE
+price = 100000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = price * 0.1
+else :
+    down_payment = price * 0.2
+#print(f"Down payment: ${down_payment}")
