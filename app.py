2cf160858ecbf894fb0fe63bd971ef0d1148ecaa
henry-benjamin <akyambahenrybenjamin@gmail.com>
2026-09-04 23:37:39 +0300

    While and I built a guess number game

--- app.py
+++ app.py
@@ -150,12 +150,37 @@ else:
     #(f" '{name_user}' Name looks good! ")
 
 # project convert weigth
-weight = int(input("Weiht: "))
-unit = input('(L)bs or (K)g: ')
+#weight = int(input("Weiht: "))
+#unit = input('(L)bs or (K)g: ')
 
-if  unit.upper() == 'L':
-    converted = weight * 0.45
-    print(f"You are : {converted} kilos") 
+#if  unit.upper() == 'L':
+    #converted = weight * 0.45
+    #print(f"You are : {converted} kilos") 
+#else:
+    #converted = weight / 0.45
+    #print(f"You are : {converted} pounds")
+
+#8 WHILE LOOPS
+#while condition:
+   #...
+   #increase
+
+i = 1
+while i<= 5:
+    #print('*' * i)
+    i += 1
+#print("Done")
+
+#GUESSING GAME
+ 
+secrete_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input("Guess: "))
+    guess_count += 1
+    if guess == secrete_number:
+        print("You Won!") 
+        break
 else:
-    converted = weight / 0.45
-    print(f"You are : {converted} pounds")
+    print("Sorry, You failed!")
